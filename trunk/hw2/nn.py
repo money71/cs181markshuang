98fff02fd329c28a730a50d475fba722ccb50271
wu.sean@gmail.com <wu.sean@gmail.com@6754367e-6c00-86e5-cd15-1446e658ef88>
2011-02-20 20:05:28 +0000

    fix

--- trunk/hw2/nn.py
+++ trunk/hw2/nn.py
@@ -172,10 +172,7 @@ def pcpt_activation(pcpt, listDblInput):
     >>> pcpt = Perceptron([0.5,0.5,-1.5], 0.75, 0)
     >>> pcpt_activation(pcpt, [0.5,1.0,1.0])
     0.5"""
-    out = 0
-    for i in range(len(listDblInput)):
-      out = out + listDblInput[i] * pcpt.listDblW[i];
-    return out
+    return sigmoid(dot(pcpt.listDblW,listDblInput))
 
 def feed_forward_layer(layer, listDblInput):
     """Build a list of activation levels for the perceptrons
