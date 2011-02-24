a78cb067a029944048716466c76ed1623c25c548
wu.sean@gmail.com <wu.sean@gmail.com@6754367e-6c00-86e5-cd15-1446e658ef88>
2011-02-24 06:01:18 +0000

    streamlined some code, updated runmany to be more robust, and added nnfast which is a turbocharged version of nn (breaks test suite though)

--- trunk/hw2/runmany.py
+++ trunk/hw2/runmany.py
@@ -2,46 +2,57 @@ import sys
 import os
 from subprocess import *
 
-iterations = 2
+iterations = 4
+depth = 2
+rounds = 100
+hidden = 0
 
-rounds = 16
+rounds_data = open("00_rounds_data.txt","w")
 
-hiddens = [0]
-
-rates = [.2, .3, .4, .5, .8, 1.0, 1.2, 1.5]
-
-rounds_data = open("rounds_data.txt","w")
-
-
-print "  rate  rnds  hide  corr  total"
-rounds_data.write("  rate  rnd   hide    train      valid\n")
+print "  rate       max"
+rounds_data.write("  rate   rnd     train     valid\n")
 if __name__=='__main__':
-  for i in range(iterations):
-    for hidden in hiddens:
-      for rate in rates:
-        thisargs =  ["python","nn.py", "-l", str(rate),"--hidden="+str(hidden),\
-                     "-n",str(rounds),"2>&1"]
-        outfile = "full_rounds%d_hidden%d_rate%f_%d.txt" % (rounds, hidden, rate, i)
-        outfd = open(outfile, "w")
-        output = Popen(thisargs, stderr=outfd,stdout=PIPE).communicate()[0]
-        outfd.close()
-        tok = output.split()
-        print "%5.2f %5d %5d %5s %5s" % (rate, rounds, hidden, tok[4], tok[7])
-        infile = outfile
-        infd = open(infile, "r")
-        infd.readline()
-        infd.readline()
-        infd.readline()
-        rnd = 0
-        for line in infd:
-          rnd += 1
-          tok = line.split(": ")
-          train = tok[1].split(',')[0]
-          val   = tok[2]
-          roundstr = "%5.2f %5d %5d %10s %10s" % (rate, rnd, hidden, train, val)
-          rounds_data.write(roundstr)
-          rounds_data.flush()
-        infd.close()
+    for i in range(iterations):
+        maxVal = [0, 0, 0.0, 0.0]
+        rate = 0.2
+        incr = 0.01
+        for d in range(depth):
+            for j in range(20):
+                curMaxVal = [0, 0, 0.0, 0.0]
+                args = ["python", "nnfast.py", "-l", str(rate),
+                        "--hidden="+str(hidden), "-n", str(rounds),
+                        "--train=training-9k.txt", "--test=test-1k.txt",
+                        "--validation=validation-1k.txt", "--num_inputs=196",
+                        "2>&1"]
+                outfile = "%d_hidden%d_rate%f.txt" % (i, hidden, rate)
+                outfd = open(outfile, "w")
+                Popen(args, stderr=outfd,stdout=PIPE).communicate()[0]
+                outfd.close()
+                infile = outfile
+                infd = open(infile, "r")
+                infd.readline()
+                infd.readline()
+                infd.readline()
+                rnd = 0
+                for line in infd:
+                    rnd += 1
+                    tok = line.split(": ")
+                    train = tok[1].split(',')[0]
+                    val   = tok[2]
+                    if curMaxVal[3] < val:
+                        curMaxVal = [rate, rnd, train, val]
+                roundstr = "%1.4f %5d  %8s  %8s" % (curMaxVal[0], curMaxVal[1],
+                                                    curMaxVal[2], curMaxVal[3])
+                rounds_data.write(roundstr)
+                rounds_data.flush()
+                infd.close()
+                if curMaxVal[3] > maxVal[3]:
+                    maxVal = curMaxVal
+                elif curMaxVal[3] == maxVal[3] and curMaxVal[1] <= maxVal[1]:
+                    maxVal = curMaxVal
+                print "%1.4f  %8s" % (curMaxVal[0], curMaxVal[3])
+                rate -= incr
+            rate = maxVal[0]+incr
+            incr = incr*0.1
 
 rounds_data.close()
-
