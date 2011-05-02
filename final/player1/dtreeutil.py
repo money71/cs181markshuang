d5303739dd4b15bd065c8436a70dc6bc8ef916cf
wu.sean <wu.sean@gmail.com>
2011-05-02 18:22:29 +0000

    player_VI now eats plants based on classification

---
+++ final/player1/dtreeutil.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+"""
+dtreeutil.py
+
+Convenience functions for accessing decision trees.
+"""
+
+
+import pickle
+import dtree
+
+    
+def dt_insts(y, x):
+    """Make a list of dt instances out of targets and data."""
+    listInst = []
+    for i in xrange(len(y)):
+        listInst.append(dtree.Instance(x[i], bool(y[i])))
+    return listInst
+
+
+def dt_list_acc(maxRounds, listInst):
+    """Get a list of testing accuracies for each round of boosting."""
+    cFold = 10
+    listTestAcc = []
+    for cRounds in xrange(1, maxRounds+1):
+        fxnGen = dt_build_fold_gen(cRounds, False)
+        score = dtree.cv_score(fxnGen(listInst, cFold))
+        print "%d rounds, %f score" % (cRounds, score)
+        listTestAcc.append(score)
+    return listTestAcc
+
+
+def dt_build_fold_gen(cRounds, fUseTraining):
+    """Build cross-validated boosting committees."""
+    def yield_folds(listInst, cFold):
+        for cvf in dtree.yield_boosted_folds(listInst, cFold):
+            cvf.cMaxRounds = cRounds
+            if fUseTraining:
+                cvf.listInstTest = cvf.listInstTraining
+            yield cvf
+    return yield_folds
+
+
+def dt_build_model(listInst, cRounds):
+    """Build boosted committee from instances"""
+    return dtree.boost(listInst, cRounds)
+
+
+def dt_save_model(filename, m):
+    """Save a trained model to file."""
+    file = open(filename, 'w')
+    pickle.dump(m, file)
+    file.close()
+
+
+def dt_load_model(filename):
+    """Retrieve a trained model from file."""
+    file = open(filename, 'r')
+    m = pickle.load(file)
+    file.close()
+    return m
+
+
+def dt_predict(data, m):
+    """Classify an input."""
+    inst = dtree.Instance(data, False)
+    if dtree.classify_boosted(m, inst):
+        return 1
+    return 0
---
+++ final/player1/nbayesutil.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+"""
+nbayesutil.py
+
+Convenience functions for accessing naive bayes.
+"""
+
+
+import pickle
+import nbayes
+from itertools import *
+
+
+def nbayes_data(y, x):
+    """Generate data that nbayes can read from targets, data."""
+    data = []
+    for yi, xi in izip(y,x):
+        xi.insert(0, yi)
+        data.append(xi)
+    return data
+
+
+def nbayes_model(data):
+    """Train a naive bayes model."""
+    m = nbayes.Bayes(data)
+    m.train()
+    return m
+
+
+def nbayes_save_model(filename, m):
+    """Save a trained model to file."""
+    file = open(filename, 'w')
+    pickle.dump(m, file)
+    file.close()
+
+
+def nbayes_load_model(filename):
+    """Retrieve a trained model from file."""
+    file = open(filename, 'r')
+    m = pickle.load(file)
+    file.close()
+    return m
+
+
+def nbayes_test(m, data):
+    """Return classification accuracy on data."""
+    numTotal = len(data)
+    numCorrect = 0.0
+    for i in xrange(numTotal):
+        if data[i][0] == nbayes_predict(data[i][1:], m):
+            numCorrect += 1
+    return numCorrect/numTotal
+
+
+def nbayes_predict(instance, m):
+    """Classify an input."""
+    return m.classify(instance)[0]
