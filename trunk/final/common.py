7733e1a500edab85320fa9f022a8fedae3b4c023
wu.sean@gmail.com <wu.sean@gmail.com@6754367e-6c00-86e5-cd15-1446e658ef88>
2011-05-03 04:58:22 +0000

    merged in new project files

--- trunk/final/common.py
+++ trunk/final/common.py
@@ -7,9 +7,10 @@ def get_move(view):
   # If there is a plant in this location, then try and eat it.
   hasPlant = view.GetPlantInfo() == game_interface.STATUS_UNKNOWN_PLANT
   # Choose a random direction
-  if hasPlant:
-    for i in xrange(5):
-      view.GetImage()
+  #if hasPlant:
+  #  for i in xrange(5):
+  #    view.GetImage()
   # Simulate taking some time to process
-  time.sleep(0.1)
-  return (random.randint(0, 3), random.random() > 0.5)
+  #time.sleep(0.1)
+  print view.GetXPos()
+  return (random.randint(0, 3), True)
